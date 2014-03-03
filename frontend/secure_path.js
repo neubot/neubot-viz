7be0548b6bafd089ac74444f9ccbe1b472b28350
giuseppefutia <giuseppe.futia@gmail.com>
2014-03-03 12:56:54 +0100

    Change comments

--- frontend/secure_path.js
+++ frontend/secure_path.js
@@ -1,27 +1,24 @@
-/** neuviz/secure_path.js
-
-#
-# Copyright (c) 2014
-#     Nexa Center for Internet & Society, Politecnico di Torino (DAUIN),
-#     Simone Basso <simone.basso@polito.it>
-#     and Giuseppe Futia <giuseppe.futia@polito.it>.
-#
-# This file is part of NeuViz.
-#
-# NeuViz is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# NeuViz is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with NeuViz.  If not, see <http://www.gnu.org/licenses/>.
-#
-
+/** neuviz/frontend/secure_path.js
+*
+* Copyright (c) 2014
+*    Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
+*    and Giuseppe Futia <giuseppe.futia@polito.it>.
+*
+* This file is part of NeuViz.
+*
+* NeuViz is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* NeuViz is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with NeuViz.  If not, see <http://www.gnu.org/licenses/>.
+*
 **/
 
 var path = require("path");
