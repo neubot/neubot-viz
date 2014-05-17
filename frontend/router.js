51c07c9a30caa4fd9daf3c35824f4d890313fac6
Simone Basso <bassosimone@gmail.com>
2014-05-17 11:58:38 +0200

    jsbeautifier.org router.js

--- frontend/router.js
+++ frontend/router.js
@@ -1,27 +1,25 @@
 /** neuviz/frontend/router.js
-*
-* Copyright (c) 2014
-*    Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
-*    and Giuseppe Futia <giuseppe.futia@polito.it>.
-*
-* This file is part of NeuViz.
-*
-* NeuViz is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* NeuViz is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with NeuViz.  If not, see <http://www.gnu.org/licenses/>.
-*
-**/
-
-
+ *
+ * Copyright (c) 2014
+ *    Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
+ *    and Giuseppe Futia <giuseppe.futia@polito.it>.
+ *
+ * This file is part of NeuViz.
+ *
+ * NeuViz is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeuViz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NeuViz.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
 var path = require("path");
 var ROOT = "/var/www/";
 var API = "/neuviz/1.0/data/";
@@ -32,7 +30,7 @@ var months = {};
 
 /**
  *
- * Set year paramaters available for the Web API  
+ * Set year paramaters available for the Web API
  *
  */
 
@@ -49,8 +47,8 @@ var fillYears = function () {
  */
 
 var fillMonths = function () {
-    for (var i = 1; i <= 12; i ++) {
-       months[i] = true;
+    for (var i = 1; i <= 12; i++) {
+        months[i] = true;
     }
 };
 
@@ -62,10 +60,10 @@ var fillMonths = function () {
  */
 
 var checkParameters = function (parameters) {
-	fillYears();
-	fillMonths();
+    fillYears();
+    fillMonths();
     var parameter = parameters.split("/");
-    if (parameter.length>2) return false;
+    if (parameter.length > 2) return false;
     if (years[parameter[0]] === undefined) return false;
     if (months[parameter[1]] === undefined) return false;
     return true;
@@ -84,13 +82,13 @@ var formatJSON = function (parameters) {
 
     /* TODO: improve the output of the python data processor
     to obtain a cleaner file name and a cleaner code */
-    
+
     var file = "result_";
     var time = parameters[1].split("/");
     if (time[1].length == 1)
-        file += "0" + time[1];  
+        file += "0" + time[1];
     else file += time[1];
-        file += "_" + time[0] + ".json";
+    file += "_" + time[0] + ".json";
     return file;
 
 }
@@ -102,34 +100,31 @@ var formatJSON = function (parameters) {
  */
 
 var route = function (pathName) {
-	var resource = undefined;
+    var resource = undefined;
 
-	if (pathName.indexOf(API, 0) === 0) {
+    if (pathName.indexOf(API, 0) === 0) {
         var mapped = path.join(ROOT, pathName);
         var pathController = path.join(ROOT, API);
 
         if (mapped.indexOf(pathController, 0) === 0) {
-            
-        	var parameters = mapped.split(pathController);
 
-        	if (checkParameters(parameters[1])) {
-               resource = ROOT + API + formatJSON(parameters);
+            var parameters = mapped.split(pathController);
+
+            if (checkParameters(parameters[1])) {
+                resource = ROOT + API + formatJSON(parameters);
             }
         }
-    } 
-    else if (pathName === "" || pathName === "/" || pathName === "index" || pathName === "index.html") {
+    } else if (pathName === "" || pathName === "/" || pathName === "index" || pathName === "index.html") {
         pathName = "/index.html";
-	    resource = path.join(ROOT, pathName);
-    }
-
-    else {
-        var staticResource = path.join(ROOT,pathName);
+        resource = path.join(ROOT, pathName);
+    } else {
+        var staticResource = path.join(ROOT, pathName);
         if (staticResource.indexOf(ROOT, 0) === 0) {
             resource = staticResource;
         }
     }
-     
+
     return resource;
-}    
+}
 
 exports.route = route;
