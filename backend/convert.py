e2f2d3201573094f3514acb6d7b60e8d466f9679
Simone Basso <bassosimone@gmail.com>
2014-05-17 22:29:55 +0200

    Pythonize convert.py a bit
    Mainly, please pylint as much as possible. Also, do not check
    whether the file exists: in Python it is more typical to try to
    open the file and let the open() raise on error.

--- backend/convert.py
+++ backend/convert.py
@@ -1,5 +1,4 @@
 import sys
-import os.path
 
 import csv
 import json
@@ -9,7 +8,7 @@ from provincefinder import create_provinces_dict
 def process(row, provinces, provincesdict):
     key_istat = int(row[0])
     name = row[1]
-    rowtable = {} 
+    rowtable = {}
     rowtable['A+'] = int(row[2])
     rowtable['A'] = int(row[3])
     rowtable['B'] = int(row[4])
@@ -24,29 +23,26 @@ def process(row, provinces, provincesdict):
         provinces[province] = {'municipalities': {}, 'values': rowtable}
         provinces[province]['values'] = rowtable
     else:
-        provinces[province]['municipalities'].update({key_istat: { 'name': name, 'values': rowtable}})
-        for key, value in provinces[province]['values'].items():
+        provinces[province]['municipalities'].update({key_istat: {
+          'name': name, 'values': rowtable}})
+        for key, _ in provinces[province]['values'].items():
             provinces[province]['values'][key] += rowtable[key]
- 
+
 def main():
     if len(sys.argv) < 2:
-        print "Usage: python convert.py [file]"
-        sys.exit()
+        sys.exit("Usage: python convert.py [file]")
 
-    if not os.path.exists(sys.argv[1]):
-        print "Error: the file specified doesn't exist"
-        sys.exit()
-
-    provinces ={} 
+    provinces = {}
     provincesdict = create_provinces_dict()
     with open(sys.argv[1], 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter = ',')
-        firstline = True;
+        reader = csv.reader(csvfile, delimiter=',')
+        firstline = True
         for row in reader:
             if firstline:
                 firstline = False
                 continue
             process(row, provinces, provincesdict)
+
     root = {'type': 'cityCollection', 'features': provinces}
     out_file = open('result.json', 'w')
     out_file.write(json.dumps(root))
