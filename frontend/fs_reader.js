8d39948a5829ced0b1db001b0985be422175199f
Simone Basso <bassosimone@gmail.com>
2014-05-17 15:02:42 +0200

    fs_reader.js: cosmetic changes

--- frontend/fs_reader.js
+++ frontend/fs_reader.js
@@ -1,25 +1,29 @@
-/** neuviz/frontend/fs_reader.js
-*
-* Copyright (c) 2014
-*    Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
-*    and Giuseppe Futia <giuseppe.futia@polito.it>.
-*
-* This file is part of NeuViz.
-*
-* NeuViz is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* NeuViz is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-* 
-* You should have received a copy of the GNU General Public License
-* along with NeuViz.  If not, see <http://www.gnu.org/licenses/>.
-*
-**/
+// frontend/fs_reader.js
+
+/*-
+ * Copyright (c) 2014
+ *    Nexa Center for Internet & Society, Politecnico di Torino (DAUIN)
+ *    and Giuseppe Futia <giuseppe.futia@polito.it>.
+ *
+ * This file is part of NeuViz.
+ *
+ * NeuViz is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NeuViz is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NeuViz.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// File system reader
+//
 
 var path = require("path");
 var utils = require("./utils");
@@ -28,14 +32,7 @@ exports.serve__ = function (pathResource, response, contentType) {
     utils.servePath__(pathResource, response, contentType);
 };
 
-/**
- *
- * Set the content type of the server.js response
- * based on the requested resource
- *
- */
-
-var reqtype = function(pathName) {
+exports.reqtype = function(pathName) {
     var extension = path.extname(pathName);
     
     if (extension === "")
@@ -49,5 +46,3 @@ var reqtype = function(pathName) {
     
     return contentType[extension];
 };
-
-exports.reqtype = reqtype;
