66c735dce8978fcc55cef2c606c92c1e22a807e9
Simone Basso <bassosimone@gmail.com>
2014-05-17 22:25:48 +0200

    provincefinder.py: please pylint
    1) Remove spaces at EOL
    
    2) Comment unused variable
    
    3) Other PEP8 things

--- backend/provincefinder.py
+++ backend/provincefinder.py
@@ -1,16 +1,16 @@
 import csv
 
 def process_(row, table):
-    province = int(row[0]) 
-    key_istat = int(row[1]) 
-    name_mun = row[2]
+    province = int(row[0])
+    key_istat = int(row[1])
+    #name_mun = row[2]
     table[key_istat] = province
 
 def create_provinces_dict():
     with open('02-impianti-termici.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile, delimiter = ';')
+        reader = csv.reader(csvfile, delimiter=';')
         table = {}
-        firstline = True;
+        firstline = True
         for row in reader:
             if firstline:
                 firstline = False
